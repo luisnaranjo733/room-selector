0ff5b98b0d092d127559ae3ee03dffd351ea5baf
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-18 03:32:13 -0700

    Added flair

--- runserver.py
+++ runserver.py
@@ -1,2 +1,16 @@
+import sys
 from roomSelector import app
-app.run(debug=True)
+
+DEBUG = True
+
+if DEBUG:
+    host = '127.0.0.1'
+    port = '5000'
+else:
+    host = '69.91.176.28'
+    port = '80'
+    
+if len(sys.argv) > 1:
+    host, port = sys.argv[1].split(':')
+
+app.run(host, int(port), debug=True)
