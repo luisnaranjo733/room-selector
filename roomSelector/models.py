ffe1b98eed5af30b46e0df0dc269775828fe98fc
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-18 04:08:24 -0700

    Solid progress

--- roomSelector/models.py
+++ roomSelector/models.py
@@ -1,21 +1,28 @@
 
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy.types import Integer, String
+from sqlalchemy.orm import relationship
 from werkzeug import generate_password_hash, check_password_hash
 
 from roomSelector.database import Base
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
     email = Column(String(120), unique=True)
     phone = Column(String(20))
 
+    house_points = Column(Integer)
+
     password_hash = Column(String(54))
-    #user_type_id = Column(Integer, ForeignKey('parent.id'))
-    #room_id = Column(Integer, ForeignKey('parent.id'))
+
+    user_type_id = Column(Integer, ForeignKey('user_type.id'))
+    user_type = relationship('UserType', backref='user')
+
+    room_id = Column(Integer, ForeignKey('room.id'))
+    room = relationship('Room', backref='user')
 
     def __init__(self, **kwargs):
         for attr in kwargs:
@@ -37,3 +44,32 @@ class User(Base):
         'Check the given password (hash) against the stored hash'
         return check_password_hash(self.password_hash, password)
 
+
+class UserType(Base):
+    __tablename__ = 'user_type'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+    def __repr__(self):
+        return '<UserType %r>' % (self.name)
+
+
+class Room(Base):
+    __tablename__ = 'room'
+    id = Column(Integer, primary_key=True)
+    house_id = Column(Integer, ForeignKey('house.id'))
+    house = relationship('House', backref='room')
+
+    def __repr__(self):
+        return '<Room %r>' % (self.id)
+
+
+class House(Base):
+    __tablename__ = 'house'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    manager_id = Column(Integer, ForeignKey('user.id'))
+    manager = relationship('User', backref='house')
+
+    def __repr__(self):
+        return '<House %r>' % (self.name)
--- shell.py
+++ shell.py
@@ -2,9 +2,18 @@
 from sys import argv
 
 from roomSelector.database import init_db, db_session
-from roomSelector.models import User
+from roomSelector.models import User, UserType, Room, House
 
 init_db()
+
+def query_all_db_items():
+    tables = [User, UserType, Room, House]
+    results = {}
+    for table in tables:
+        results[table.__tablename__] = table.query.all()
+    return results
+
+
 
 if __name__ == '__main__':
     if len(argv) == 1:
@@ -16,17 +25,44 @@ if __name__ == '__main__':
     flag = argv[1]
 
     if flag == '-c':
+        ut = UserType()
+        ut.name = 'admin'
+        db_session.add(ut)
+
+
         u = User(name='luis')
+        u.user_type = ut
         db_session.add(u)
+
+
+        h = House()
+        h.name =  'phi delts'
+        h.manager = u
+        db_session.add(h)
+
+
+        r =  Room()
+        r.house = h
+        db_session.add(r)
+
         db_session.commit()
 
     elif flag == '-l':
-        print('Users:')
-        for user in User.query.all():
-            print('\t%r (%s)' % (user, user.phone))
-        print('')
+        db_items = query_all_db_items()
+        for item in db_items:
+            print item.upper()
+            print db_items[item]
+            print ' '
+
     elif flag == '-d':
         User.query.delete()
+        UserType.query.delete()
         db_session.commit()
-else:
-    luis = User.query.filter(User.name == 'Luis Naranjo').first()
+
+    exit(0)
+
+items = query_all_db_items()
+h = items['house'][0]
+r = items['room'][0]
+u = items['user'][0]
+ut = items['user_type'][0]
